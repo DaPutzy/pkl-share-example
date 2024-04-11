c6f63716566dd51cd490dea31ef04b909995c734
DaPutzy <9727551+DaPutzy@users.noreply.github.com>
2024-04-11 11:07:16 +0200

    only eval once

--- generate.sh
+++ generate.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-for filename in $(find clusters -name 'generator.pkl' -type 'f'); do
-    dir=$(dirname "$filename")
-    pkl eval -m "$dir" "$filename"
-done
+pkl eval -m . generate.pkl
